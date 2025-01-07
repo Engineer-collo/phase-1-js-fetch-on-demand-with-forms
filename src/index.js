699a473d5ae0ed327a564601df1e302c9a3de26c
Engineer-Collo <collinslikhomba@gmail.com>
2025-01-07 09:19:24 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+      
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+       .then(res => res.json())
+       .then(data => {
+        
+
+       const title = document.querySelector("#h4");
+       const summary = document.querySelector("#p");
+
+       title.innerHTML = data.title
+       summary.innerHTML = data.summary}
+      );
+      input.value = ""
+
+             });
+    
+};
+
+document.addEventListener("DOMContentLoaded", init);
